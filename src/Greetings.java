41bbb65fe5d8480214fa4c1357fdcc38911710d6
“Mamofm” <jmamo603@gmail.com>
2021-10-04 19:55:45 -0400

    first commit

---
+++ src/Greetings.java
@@ -0,0 +1,9 @@
+public class Greetings {
+    static void greeting(int x) {
+        System.out.printf("Hello %d", x);
+    }
+
+    public static void main(String[] args) {
+        greeting(3);
+    }
+}
